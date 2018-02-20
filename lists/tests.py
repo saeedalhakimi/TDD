2cf156d138ad3e00ab13fd51c7afe795a147782a
Saeed Al Hakimi <saeed.alhakimi@live.com>
2018-02-20 15:13:50 +0300

    Functional test now checks we can input a to-do

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,12 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 # Create your tests here.
+
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
